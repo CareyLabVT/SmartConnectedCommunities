7bfcbb44ed26acea9599ca927a16944ad26ac95f
Vahid Daneshmand <v.daneshmand@gmail.com>
2022-05-20 11:21:22 -0400

    Update forecast-graph.js

--- assets/js/forecast-graph.js
+++ assets/js/forecast-graph.js
@@ -1,1 +1,18 @@
-document.getElementById("forecast").src='https://s3.flare-forecast.org/analysis/fcre/fcre-'+(new Date(new Date().getTime() - 24*60*60*1000)).toJSON().slice(0,10)+'-fcre_js2_turnover.png';
+today-graph = 'https://s3.flare-forecast.org/analysis/fcre/fcre-'+(new Date(new Date().getTime() - 24*60*60*1000)).toJSON().slice(0,10)+'-fcre_js2_turnover.png';
+yesterday-graph = 'https://s3.flare-forecast.org/analysis/fcre/fcre-'+(new Date(new Date().getTime() - 48*60*60*1000)).toJSON().slice(0,10)+'-fcre_js2_turnover.png';
+
+function checkImage(url) {
+  var request = new XMLHttpRequest();
+  request.open("GET", url, true);
+  request.send();
+  request.onload = function() {
+    status = request.status;
+    if (request.status == 200) //if(statusText == OK)
+    {
+      document.getElementById("forecast").src = today-graph;
+    } else {
+      document.getElementById("forecast").src = yesterday-graph;
+    }
+  }
+}
+checkImage(today-graph);
