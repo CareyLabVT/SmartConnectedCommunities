bffb1f9db82983c5489688fdd833f5bd30068cd8
Vahid Daneshmand <v.daneshmand@gmail.com>
2022-05-26 15:44:25 -0400

    Update forecast-graph.js

--- assets/js/forecast-graph.js
+++ assets/js/forecast-graph.js
@@ -1,4 +1,4 @@
-function setImage(num_days) { //Look for the latest graph url
+function setImage(num_days) { //Look for the latest graph URL
   var request = new XMLHttpRequest();
   var url = 'https://s3.flare-forecast.org/analysis/fcre/fcre-'+(new Date(new Date().getTime() - num_days*24*60*60*1000)).toJSON().slice(0,10)+'-fcre_js2_turnover.png'
   request.open("GET", url, true);
@@ -9,8 +9,8 @@ function setImage(num_days) { //Look for the latest graph url
     {
       document.getElementById("forecast").src = url;
     } else {
-      setImage(num_days + 1)
+      setImage(num_days + 1) //Look for the graph URL for the previous day
     }
   }
 }
-setImage(1);
+setImage(0);
